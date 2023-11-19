5f6c743c4b496132f4736d6e2542d4b1c1867c25
Javier Macias-Guarasa <javier.maciasguarasa@uah.es>
2023-11-19 01:08:53 +0100

    Minor changes

--- sample/go.run-me.sh
+++ sample/go.run-me.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 # Silly script to show how it works with a real example
-python pdf-form-identify-fields.py GEF30-UAH.pdf -o GEF30-UAH.sh
-python pdf-form-fill.py GEF30-UAH.pdf --ini GEF30-UAH.ini
-
+python ../pdf-form-identify-fields.py GEF30-UAH.pdf -o GEF30-UAH.sh
+python ../pdf-form-fill.py GEF30-UAH.pdf --ini GEF30-UAH.ini
