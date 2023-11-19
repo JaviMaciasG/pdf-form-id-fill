1069df4bba8824f0115eea0a8e4d068f8e8c4b76
Javier Macias-Guarasa <javier.maciasguarasa@uah.es>
2023-11-19 01:02:52 +0100

    Updated requirements, removed args

---
+++ sample/go.run-me.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Silly script to show how it works with a real example
+python pdf-form-identify-fields.py GEF30-UAH.pdf -o GEF30-UAH.sh
+python pdf-form-fill.py GEF30-UAH.pdf --ini GEF30-UAH.ini
+
