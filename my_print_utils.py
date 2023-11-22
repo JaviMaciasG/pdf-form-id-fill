8cd923a09af0027225941eabc685b3c627ed5c37
Javier Macias-Guarasa <javier.maciasguarasa@uah.es>
2023-11-22 13:32:19 +0100

    Added continue option

--- my_print_utils.py
+++ my_print_utils.py
@@ -1,11 +1,14 @@
-def print_err(message):
+def print_err(message, cont=False):
+    prefix = "     " if cont else "[ERR]"
     # ANSI escape code for red
-    print(f"\033[91m[ERR] {message}\033[0m")
+    print(f"\033[91m{prefix} {message}\033[0m")
 
-def print_inf(message):
+def print_inf(message, cont=False):
+    prefix = "     " if cont else "[INF]"
     # ANSI escape code for green
-    print(f"\033[92m[INF] {message}\033[0m")
+    print(f"\033[92m{prefix} {message}\033[0m")
 
-def print_war(message):
+def print_war(message, cont=False):
+    prefix = "     " if cont else "[WAR]"
     # ANSI escape code for green
-    print(f"\033[93m[WAR] {message}\033[0m")
+    print(f"\033[93m{prefix} {message}\033[0m")
